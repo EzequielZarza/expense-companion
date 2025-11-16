535c7b8bf7e512b6703a5b95ce2331cc2bce0ce7
EzequielZarza <ezequielm.zarza@gmail.com>
2025-11-16 10:44:02 -0300

    error handling added

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,39 +1,50 @@
 import { Expense } from "../models/expenseModel.js";
 
 export const getExpenses = async (req, res) => {
-  const expenses = await Expense.find()
-  console.log('las expenses',expenses)
+  const expenses = await Expense.find();
   res.send(expenses);
 }
 
-export const getExpense = async ({params}, res) => {
-  const { id } = params
-  const expense = await Expense.findById(id)
-  res.send(expense)
+export const getExpense = async ({params: { id }}, res) => {
+  try {
+    const expense = await Expense.findById(id)
+    res.send(expense)
+  } catch(error){
+    res.status(400).json({ message: `Couldn't find the requested expense due to: ${error}`});
+  }
 }
 
 export const addExpense = async ({body}, res) => {
-  console.log('el body', body)
-  const { payer, description, amount, currency, splitType } = body;
-  const newExpense = await Expense.create({
-    payer,
-    description,
-    amount,
-    currency,
-    splitType,
-    date: Date.now()
-  });
-  res.send(newExpense)
+  try {
+    const { payer, description, amount, currency, splitType } = body;
+    const newExpense = await Expense.create({
+      payer,
+      description,
+      amount,
+      currency,
+      splitType,
+      date: Date.now()
+    });
+    res.send(newExpense)
+  } catch(error){
+    res.status(400).json({ message: `Couldn't add expense due to: ${error}`});
+  }
 }
 
-export const updatedExpense = async ({body: updates, params}, res) => {
-  const { id } = params
-  const updatedExpense = await Expense.findByIdAndUpdate(id, updates, { new: true})
-  res.send(updatedExpense)
+export const updatedExpense = async ({body: updates, params: { id }}, res) => {
+  try{
+    const updatedExpense = await Expense.findByIdAndUpdate(id, updates, { new: true});
+    res.send(updatedExpense);
+  }catch(error){
+    res.status(400).json({ message: `Couldn't update expense due to: ${error}`});
+  }
 }
 
-export const deleteExpense = async ({ params }, res) => {
-  const { id } = params
-  const deletedProduct = await Expense.findByIdAndDelete(id)
-  res.send(`Product deleted successfully: ${JSON.stringify(deletedProduct)}`)
+export const deleteExpense = async ({params: { id }}, res) => {
+  try{
+    const deletedProduct = await Expense.findByIdAndDelete(id);
+    res.send(`Product deleted successfully: ${JSON.stringify(deletedProduct)}`);
+  }catch(error){
+    res.status(400).json({ message: `Couldn't delete expense due to: ${error}`});
+  }
 }
